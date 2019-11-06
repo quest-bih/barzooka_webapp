c3db156da7fe9be83f857bb8d59292db1bbc2126
Nico Riedel <nico.riedel@bihealth.de>
2019-11-06 11:32:59 +0100

    Change iiif server for pdf preview in webapp to local iiif server

--- static/preview.js
+++ static/preview.js
@@ -29,7 +29,7 @@ function retrieve_previews(id, all_pages = false) {
       pvt
         .find("img")
         .hide()
-        .attr("src", "https://iiif-biorxiv.saladi.org/iiif/2/biorxiv:" + id + ".pdf/full/250,/0/default.jpg?page=" + pg)
+        .attr("src", "http://localhost:8182/iiif/2/biorxiv:" + id + ".pdf/full/250,/0/default.jpg?page=" + pg)
         .on("load", function() {
           $(this).show().parent().find(".placeholder").remove();
         });
